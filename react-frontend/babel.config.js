55f41a2b58b580c4df26f90f98a0725261983eeb
ngare ezra <ndemoezra@gmail.com>
2024-10-21 14:57:54 -0500

    Updated packages

--- react-frontend/babel.config.js
+++ react-frontend/babel.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
+  presets: [
+    "@babel/preset-env", // Transform modern JS
+    "@babel/preset-react" // Transform JSX and React-specific syntax
+  ],
 };
