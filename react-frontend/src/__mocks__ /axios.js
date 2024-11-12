9bfec9538b120d7465de4f25411f220da902fdd4
ezra ngare <ezra.n.ngare@vanderbilt.edu>
2024-11-12 00:12:22 -0600

    profile tests

--- react-frontend/src/__mocks__ /axios.js
+++
@@ -1,7 +0,0 @@
-// __mocks__/axios.js
-export default {
-    get: jest.fn(() => Promise.resolve({ data: {} })),
-    post: jest.fn(() => Promise.resolve({ data: {} })),
-    // Add more methods if your tests use other axios functions
-  };
-  
