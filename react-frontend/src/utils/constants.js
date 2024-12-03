de09ee508b805328b6a01508f04d38c3e7ffc4f0
Farouk Haroun <75143775+farouk-haroun@users.noreply.github.com>
2024-12-03 11:13:32 -0600

    Update constants.js

--- react-frontend/src/utils/constants.js
+++ react-frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-//export const API_BASE_URL = 'https://won-dry-idea-platform.onrender.com/api';
-export const API_BASE_URL = 'http://localhost:9000/api';
+export const API_BASE_URL = 'https://won-dry-idea-platform.onrender.com/api';
+// export const API_BASE_URL = 'http://localhost:9000/api';
